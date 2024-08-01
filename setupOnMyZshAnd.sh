1227b2bf896396df1964b61913e5914babcbc0eb
jay yang <jayyang@lucidmotors.com>
2024-08-01 15:16:44 -0700

    add .zsh_history

--- setupOnMyZshAnd.sh
+++ setupOnMyZshAnd.sh
@@ -8,11 +8,11 @@ sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/too
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-#open ~/.bashrc -> zsh-syntax-highlighting zsh-autosuggestions int plugins {git}, then changes ZSH_THEME="powerlevel10k/powerlevel10k"
+#open ~/.bashrc -> zsh-syntax-highlighting z zsh-autosuggestions web-search into plugins {git}, then changes ZSH_THEME="powerlevel10k/powerlevel10k"
 
 mkdir -p ~/.fonts
 wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.0.2/Meslo.zip
-unzip Meslo.zip ~/.fonts
+unzip Meslo.zip ~/.fonts, double click of files, to have MesloLGS Nerd Font Mono Regular installed
 #uzip the above fonts files to ~/.fonts
 #or
 #curl -L https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Regular.ttf --output ~/.fonts/'MesloLGS NF Regular.ttf'
@@ -20,3 +20,11 @@ unzip Meslo.zip ~/.fonts
 #curl -L https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Italic.ttf --output ~/.fonts/'MesloLGS NF Italic.ttf'
 #curl -L https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Bold%20Italic.ttf --output ~/.fonts/'MesloLGS NF Bold Italic.ttf'
 #curl -L https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Regular.ttf --output ~/.fonts/'MesloLGS NF Regular.ttf'
+
+in tweaks,
+Fonts -> MesloLGS Nerd Font Mono Regular
+
+in Vs Code,
+ctrl + ,
+in settings, terminal.integrated.font, put the "MesloLGS Nerd Font Mono"
+
